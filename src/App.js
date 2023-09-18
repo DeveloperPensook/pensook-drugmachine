351543a84d1c34a278e7b73baf62309c7ebe156b
jiramethnate <jiramethneatpatiparn@gmail.com>
2023-09-18 17:26:17 +0700

    Test: App.js - Receive socket data from services (API)addStock

--- src/App.js
+++ src/App.js
@@ -6,36 +6,41 @@ import ShowPin from "./pages/ShowPin.jsx";
 import StockList from "./pages/StockList.jsx";
 import Success from "./pages/Success.jsx";
 
-const socket = io('http://localhost:4000');
-
 function App() {
-  const [messages, setMessages] = useState([]);
   const [currentPage, setCurrentPage] = useState(null);
+  const [socketData, setSocketData] = useState({ page: null });
 
   useEffect(() => {
-    socket.on('message', (newMessage) => {
-      const parsedMessage = JSON.parse(newMessage);
-      setMessages([...messages, parsedMessage.text]);
+    const socket = io('http://localhost:4000');
 
-      switch (parsedMessage.page) {
-        case 'idle':
-          setCurrentPage(<IdlePage />);
-          break;
-        case 'show-pin':
-          setCurrentPage(<ShowPin />);
-          break;
-        case 'stock-list':
-          setCurrentPage(<StockList />);
-          break;
-        case 'success':
-          setCurrentPage(<Success />);
-          break;
-        default:
-          setCurrentPage(null);
-          break;
-      }
+    socket.on("message", (data) => {
+      setSocketData(JSON.parse(data));
     });
-  }, [messages]);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    switch (socketData.page) {
+      case 'idle':
+        setCurrentPage(<IdlePage />);
+        break;
+      case 'show-pin':
+        setCurrentPage(<ShowPin />);
+        break;
+      case 'stock-list':
+        setCurrentPage(<StockList />);
+        break;
+      case 'success':
+        setCurrentPage(<Success />);
+        break;
+      default:
+        setCurrentPage(null);
+        break;
+    }
+  }, [socketData]);
 
   return (
     <div className="App">
