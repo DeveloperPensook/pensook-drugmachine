bafc0670c77726a233a18e0e98f663a75860e07a
jiramethnate <135225542+jiramethnate@users.noreply.github.com>
2023-10-08 22:19:21 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import ShowPin from "./pages/ShowPin.jsx";
 import StockList from "./pages/StockList.jsx";
 import Success from "./pages/Success.jsx";
 import Login from "./pages/Login.jsx";
+import Failed from "./pages/Failed.jsx";
 import Cookies from 'js-cookie';
 
 function App() {
   const [currentPage, setCurrentPage] = useState(<IdlePage />);
   const [socketData, setSocketData] = useState({ page: null });
+  const [socketData2, setSocketData2] = useState({ page: null });
 
   useEffect(() => {
     const socket = io(
@@ -30,6 +32,19 @@ function App() {
   }, []);
 
   useEffect(() => {
+    const socket2 = io(`http://localhost:6007`);
+
+    socket2.on("message", (data) => {
+      setSocketData2(JSON.parse(data));
+      console.log(JSON.parse(data))
+    });
+
+    return () => {
+      socket2.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
     const sessionValue = Cookies.get('session');
     if (sessionValue) {
       const decodedString = decodeURIComponent(sessionValue);
@@ -41,6 +56,7 @@ function App() {
               setCurrentPage(<ShowPin socketMessage={socketData.message} />);
               break;
             case 'stock-list':
+              Object.assign(socketData.message, { drugMachineCode: cookieSession.Code })
               setCurrentPage(<StockList socketMessage={socketData.message} />);
               break;
             case 'success':
@@ -50,6 +66,28 @@ function App() {
               setCurrentPage(null);
               break;
           }
+        }
+      } else if (cookieSession.Code && socketData2.drugMachineCode) {
+        if (cookieSession.Code == socketData2.drugMachineCode) {
+          const requestData = {
+            ...(socketData2.stockLedgerEntryId
+              ? { stockLedgerEntryId: socketData2.stockLedgerEntryId }
+              : { drugMachineOpenHistoryId: socketData2.drugMachineOpenHistoryId }),
+          };
+          
+          const BACKEND_URL = process.env.REACT_APP_IS_PROD  == 'true'
+          ? process.env.REACT_APP_BACKEND_URL_PROD
+          : process.env.REACT_APP_BACKEND_URL
+
+        axios
+          .post(`${BACKEND_URL}/api/stockLedger/updateStatusToApproved`, requestData)
+          .then((response) => {
+            console.log('POST request successful:', response.data);
+            setCurrentPage(<Success />);
+          })
+          .catch((error) => {
+            console.error('Error making POST request:', error);
+          });
         }
       } else {
         setCurrentPage(<IdlePage/>)
--- src/pages/StockList.jsx
+++ src/pages/StockList.jsx
@@ -1,8 +1,48 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from 'js-cookie';
 
 function StockList({ socketMessage }) {
-  const [pin, setPin] = useState('');
+  const [pin, setPin] = useState("");
+
+  useEffect(() => {
+    const sessionValue = Cookies.get("session");
+    const decodedString = decodeURIComponent(sessionValue);
+    const cookieSession = JSON.parse(decodedString);
+    let address = [];
+    let value = [];
+    if (socketMessage.entryType == "Pickup Medicine") {
+      socketMessage.stockList.forEach(function (row) {
+        address.push(row.slotAddress)
+        value.push(row.qty)
+      })
+    } else {
+      address = [`${cookieSession.openDoorAddress}`]
+      value = ["1"]
+    }
+    let requestData = {
+      ip: cookieSession.ipAddress,
+      modbusPort: cookieSession.port,
+      slaveId: cookieSession.slaveId,
+      address: address,
+      value: value,
+      drugMachineCode: socketMessage.drugMachineCode,
+      stockLedgerEntryId: socketMessage.stockLedgerEntryId,
+      drugMachineOpenHistoryId: socketMessage.drugMachineOpenHistoryId,
+      entryStatusAddress: cookieSession.entryStatusAddress
+    }
+    axios
+      .post(
+        `http://localhost:6007/api/stockLedger/drugMachineModbus`,
+        requestData
+      )
+      .then((response) => {
+        console.log("POST request successful:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error making POST request:", error);
+      });
+  });
 
   const updateStatusToApproved = () => {
     const requestData = {
@@ -10,17 +50,21 @@ function StockList({ socketMessage }) {
         ? { stockLedgerEntryId: socketMessage.stockLedgerEntryId }
         : { drugMachineOpenHistoryId: socketMessage.drugMachineOpenHistoryId }),
     };
-    const BACKEND_URL = process.env.REACT_APP_IS_PROD  == 'true'
-      ? process.env.REACT_APP_BACKEND_URL_PROD
-      : process.env.REACT_APP_BACKEND_URL
+    const BACKEND_URL =
+      process.env.REACT_APP_IS_PROD == "true"
+        ? process.env.REACT_APP_BACKEND_URL_PROD
+        : process.env.REACT_APP_BACKEND_URL;
 
     axios
-      .post(`${BACKEND_URL}/api/stockLedger/updateStatusToApproved`, requestData)
+      .post(
+        `${BACKEND_URL}/api/stockLedger/updateStatusToApproved`,
+        requestData
+      )
       .then((response) => {
-        console.log('POST request successful:', response.data);
+        console.log("POST request successful:", response.data);
       })
       .catch((error) => {
-        console.error('Error making POST request:', error);
+        console.error("Error making POST request:", error);
       });
   };
 
@@ -28,14 +72,18 @@ function StockList({ socketMessage }) {
     <div>
       <div>Stock List Page Content</div>
       {socketMessage.drugMachineOpenHistoryId ? (
-        <div>drugMachineOpenHistoryId: {socketMessage.drugMachineOpenHistoryId}</div>
+        <div>
+          drugMachineOpenHistoryId: {socketMessage.drugMachineOpenHistoryId}
+        </div>
       ) : (
         <div>
           <div>stockLedgerEntryId: {socketMessage.stockLedgerEntryId}</div>
           <div>stockList: {JSON.stringify(socketMessage.stockList)}</div>
         </div>
       )}
-      <button onClick={updateStatusToApproved}>Update Status to Approved</button>
+      <button onClick={updateStatusToApproved}>
+        Update Status to Approved
+      </button>
     </div>
   );
 }
