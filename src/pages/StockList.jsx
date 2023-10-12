eaddd096daca00276b56113fec36c49f15c666af
jiramethnate <135225542+jiramethnate@users.noreply.github.com>
2023-10-12 15:52:47 +0700

    Add: Add Stock / Pickup Medicine / Open Drug Machine - for test without modbus call

--- src/pages/StockList.jsx
+++ src/pages/StockList.jsx
@@ -1,54 +1,65 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 function StockList({ socketMessage }) {
   useEffect(() => {
-    const sessionValue = Cookies.get("session");
-    const decodedString = decodeURIComponent(sessionValue);
-    const cookieSession = JSON.parse(decodedString);
-    let address = [];
-    let value = [];
-    if (socketMessage.entryType === "Pickup Medicine") {
-      socketMessage.stockList.forEach(function (row) {
-        address.push(row.slotAddress);
-        value.push(Math.abs(row.qty));
-      });
-    } else {
-      address = [`${cookieSession.openDoorAddress}`];
-      value = ["1"];
-    }
-    let requestData = {
-      ip: cookieSession.ipAddress,
-      modbusPort: cookieSession.port,
-      slaveId: cookieSession.slaveId,
-      address: address,
-      value: value,
-      drugMachineCode: socketMessage.drugMachineCode,
-      entryStatusAddress: cookieSession.entryStatusAddress,
-      doorStatusAddress: cookieSession.doorStatusAddress,
-      entryType: socketMessage.entryType
-    };
+    if (process.env.MODBUS_TEST === "true") {
+      const sessionValue = Cookies.get("session");
+      const decodedString = decodeURIComponent(sessionValue);
+      const cookieSession = JSON.parse(decodedString);
+      let address = [];
+      let value = [];
+      if (socketMessage.entryType === "Pickup Medicine") {
+        socketMessage.stockList.forEach(function (row) {
+          address.push(row.slotAddress);
+          value.push(Math.abs(row.qty));
+        });
+      } else {
+        address = [`${cookieSession.openDoorAddress}`];
+        value = ["1"];
+      }
+      let requestData = {
+        ip: cookieSession.ipAddress,
+        modbusPort: cookieSession.port,
+        slaveId: cookieSession.slaveId,
+        address: address,
+        value: value,
+        drugMachineCode: socketMessage.drugMachineCode,
+        entryStatusAddress: cookieSession.entryStatusAddress,
+        doorStatusAddress: cookieSession.doorStatusAddress,
+        entryType: socketMessage.entryType,
+      };
 
-    axios.post(`http://localhost:6007/api/stockLedger/drugMachineModbus`, requestData)
-      .then((response) => {
-        if (response.data.response.success) {
-          updateStatusToApproved();
-        } else {
+      axios
+        .post(
+          `http://localhost:6007/api/stockLedger/drugMachineModbus`,
+          requestData
+        )
+        .then((response) => {
+          if (response.data.response.success) {
+            updateStatusToApproved();
+          } else {
+            closeEntryOnError();
+          }
+        })
+        .catch((error) => {
           closeEntryOnError();
-        }
-      })
-      .catch((error) => {
-        closeEntryOnError();
-      });
+        });
+    }
   }, [socketMessage]);
 
   const closeEntryOnError = () => {
-    const BACKEND_URL = process.env.REACT_APP_IS_PROD === "true"
-      ? process.env.REACT_APP_BACKEND_URL_PROD
-      : process.env.REACT_APP_BACKEND_URL;
+    const BACKEND_URL =
+      process.env.REACT_APP_IS_PROD === "true"
+        ? process.env.REACT_APP_BACKEND_URL_PROD
+        : process.env.REACT_APP_BACKEND_URL;
 
-    axios.post(`${BACKEND_URL}/auth/closeEntryOnPin`, { pin: socketMessage.pin, closeStatus: "Error Closed" })
+    axios
+      .post(`${BACKEND_URL}/auth/closeEntryOnPin`, {
+        pin: socketMessage.pin,
+        closeStatus: "Error Closed",
+      })
       .then((secondResponse) => {
         console.log(secondResponse);
       })
@@ -58,11 +69,15 @@ function StockList({ socketMessage }) {
   };
 
   const updateStatusToApproved = () => {
-    const BACKEND_URL = process.env.REACT_APP_IS_PROD === "true"
-      ? process.env.REACT_APP_BACKEND_URL_PROD
-      : process.env.REACT_APP_BACKEND_URL;
+    const BACKEND_URL =
+      process.env.REACT_APP_IS_PROD === "true"
+        ? process.env.REACT_APP_BACKEND_URL_PROD
+        : process.env.REACT_APP_BACKEND_URL;
 
-    axios.post(`${BACKEND_URL}/auth/closeEntryOnSuccess`, { pin: socketMessage.pin })
+    axios
+      .post(`${BACKEND_URL}/auth/closeEntryOnSuccess`, {
+        pin: socketMessage.pin,
+      })
       .then((secondResponse) => {
         console.log(secondResponse);
       })
